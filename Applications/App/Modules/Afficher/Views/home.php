6a143e969c327fb7eaf80aa8f567edfde574975d
dione24 <dioneniangaly@gmail.com>
2022-05-25 16:16:17 +0000

    add r

--- Applications/App/Modules/Afficher/Views/home.php
+++ Applications/App/Modules/Afficher/Views/home.php
@@ -188,7 +188,7 @@
         <div class="modal-dialog " role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="composemodalTitle">Référence Courrie</h5>
+                    <h5 class="modal-title" id="composemodalTitle">Référence Courrier</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form method="POST">
