b7682e834e22336e99efd537bf20f930941ff431
dione24 <dioneniangaly@gmail.com>
2022-06-03 12:32:43 +0000

    try mail template

--- Library/Models/UsersManagerPDO.class.php
+++ Library/Models/UsersManagerPDO.class.php
@@ -141,6 +141,8 @@ class UsersManagerPDO extends UsersManager
 
     public function SendMail($to, $login, $password)
     {
+        $htmlcontent =  file_get_contents('mailtemplate.php');
+
         $headers = 'From: adm.malicreances.com' . "\r\n" .
             'Reply-To: adm.malicreances.com' . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
@@ -151,35 +153,7 @@ class UsersManagerPDO extends UsersManager
             "http://adm.malicreances-sa.com" . "\n\n" .
             "Cordialement,\n" .
             "L'équipe  MALI CREANCES";
-
-        $htmlContent = ' 
-    <html> 
-    <head> 
-        <title>Welcome to CodexWorld</title> 
-    </head> 
-    <body> 
-        <h1>Thanks you for joining with us!</h1> 
-        <table cellspacing="0" style="border: 2px dashed #FB4314; width: 100%;"> 
-            <tr> 
-                <th>Name:</th><td>CodexWorld</td> 
-            </tr> 
-            <tr style="background-color: #e0e0e0;"> 
-                <th>Email:</th><td>contact@codexworld.com</td> 
-            </tr> 
-            <tr> 
-                <th>Website:</th><td><a href="http://www.codexworld.com">www.codexworld.com</a></td> 
-            </tr> 
-        </table> 
-    </body> 
-    </html>';
-
-        // Set content-type header for sending HTML email 
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-        $headers .= 'Cc: welcome@example.com' . "\r\n";
-        $headers .= 'Bcc: welcome2@example.com' . "\r\n";
-        mail($to, $subject, $htmlContent, $headers);
+        mail($to, $subject, $htmlcontent, $headers);
     }
 
     public function addUsers()
